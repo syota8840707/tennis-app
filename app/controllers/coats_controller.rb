e8cccee5cd4e7a0450ba2e64cbc49f1341234e50
syota8840707 <za.mazyan8840707@gmail.com>
2021-07-04 16:24:50 +0900

    コート投稿機能

--- app/controllers/coats_controller.rb
+++ app/controllers/coats_controller.rb
@@ -1,11 +1,27 @@
 class CoatsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
-    @coats = Coat.new
+    @costs = Coat.all
+    # @coat = Item.order('created_at DESC')
   end
 
   def new
+    @coat = Coat.new
   end
 
-  def save
+  def create
+    @coat = Coat.new(coat_params)
+    if @coat.save
+      redirect_to root_path      
+    else
+      render :new
+    end  
   end
+
+  private
+  def coat_params
+    params.require(:coat).permit(:image, :name, :prefecture_id, :city, :address, :start_time_id, :finish_time_id, :number_id, :info).merge(user_id: current_user.id)
+                                
+  end  
 end
--- app/models/coat.rb
+++ app/models/coat.rb
@@ -1,4 +1,17 @@
 class Coat < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :start_time
+  belongs_to_active_hash :finish_time
+  belongs_to_active_hash :number
+  
   belongs_to :user
-  has_many :comments
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :name, :info, :image, :city, :address, :user_id
+    validates :prefecture_id, :start_time_id, :finish_time_id,
+              :number_id, numericality: { other_than: 1, message: 'のセレクタから選択して下さい' }
+  end
+
 end
