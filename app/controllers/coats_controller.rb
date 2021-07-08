c5ed6520d4c502c5c148567625c6a0b4c75388bb
syota8840707 <za.mazyan8840707@gmail.com>
2021-07-08 21:03:56 +0900

    編集機能修正改善

--- app/controllers/coats_controller.rb
+++ app/controllers/coats_controller.rb
@@ -1,12 +1,7 @@
 class CoatsController < ApplicationController
-<<<<<<< HEAD
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_coat, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, only: [:edit, :destroy]
-=======
-  before_action :authenticate_user!, only: [:new, :create]
-
->>>>>>> parent of 24597e8 (編集機能実施)
   def index
     @costs = Coat.all
     @coats = Coat.order('created_at DESC')
@@ -26,11 +21,12 @@ class CoatsController < ApplicationController
   end
 
   def show
-    
-    @coat = Coat.find(params[:id])
+    @coats = Coat.all
   end
 
-<<<<<<< HEAD
+  def edit
+  end
+
   def update  
     if @coat.update(coat_params)
       redirect_to coat_path
@@ -43,21 +39,16 @@ class CoatsController < ApplicationController
     @coat.destroy
     redirect_to root_path
   end
-=======
->>>>>>> parent of 24597e8 (編集機能実施)
   private
 
   def coat_params
     params.require(:coat).permit(:image, :name, :prefecture_id, :city, :address, :start_time_id, :finish_time_id, :number_id,
                                  :info).merge(user_id: current_user.id)
   end
-<<<<<<< HEAD
   def set_coat
     @coat = Coat.find(params[:id])
   end
   def move_to_index
     redirect_to action: :index unless current_user.id == @coat.user_id
   end
-=======
->>>>>>> parent of 24597e8 (編集機能実施)
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: "coats#index"
-<<<<<<< HEAD
   resources :coats
-=======
-  resources :coats, only: [:index, :new, :create, :show]
->>>>>>> parent of 24597e8 (編集機能実施)
 end
