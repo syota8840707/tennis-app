24597e8803058cfbb3fb81e298a81826bf47074e
syota8840707 <za.mazyan8840707@gmail.com>
2021-07-08 19:06:59 +0900

    編集機能実施

--- app/controllers/coats_controller.rb
+++ app/controllers/coats_controller.rb
@@ -1,6 +1,6 @@
 class CoatsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
-
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_coat, only: [:show, :edit, :update]
   def index
     @costs = Coat.all
     @coats = Coat.order('created_at DESC')
@@ -20,14 +20,26 @@ class CoatsController < ApplicationController
   end
 
   def show
-    
-    @coat = Coat.find(params[:id])
+    @coats = Coat.all
   end
 
+  def edit
+  end
+
+  def update  
+    if @coat.update(coat_params)
+      redirect_to coat_path
+    else
+      render :edit
+    end
+  end
   private
 
   def coat_params
     params.require(:coat).permit(:image, :name, :prefecture_id, :city, :address, :start_time_id, :finish_time_id, :number_id,
                                  :info).merge(user_id: current_user.id)
+  end
+  def set_coat
+    @coat = Coat.find(params[:id])
   end
 end
