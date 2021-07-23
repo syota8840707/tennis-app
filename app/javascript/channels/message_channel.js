e898b23b307549eb42886f02f7308b5e2900398c
syota8840707 <za.mazyan8840707@gmail.com>
2021-07-23 16:08:12 +0900

    コメント名追加

--- app/javascript/channels/message_channel.js
+++ app/javascript/channels/message_channel.js
@@ -10,7 +10,7 @@ consumer.subscriptions.create("MessageChannel", {
   },
 
   received(data) {
-    const html = `<p>${data.content.message}</p>`;
+    const html = `<p>${data.username}  :  ${data.content.message}</p>`;
     const messages = document.getElementById('messages');
     const newMessage = document.getElementById('content');
     console.log(newMessage.value)
